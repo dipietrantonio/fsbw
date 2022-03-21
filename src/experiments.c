f1f24d88e7c128ee4df60a850f5b6363b4e8732f
Cristian Di Pietrantonio <cristiandipietrantonio@gmail.com>
2022-03-21 15:55:10 +0800

    Adds stddev to the output.

--- src/experiments.c
+++ src/experiments.c
@@ -12,6 +12,7 @@
 #define WRITE 0
 
 
+
 float timed_linux_read_write(int block_index, ProgramOptions *opts, short is_read){
     struct timespec begin, end;
     int fd = open(opts->filename, O_RDWR | O_FSYNC); // | __O_DIRECT);
@@ -59,17 +60,17 @@ float timed_posix_read_write(int block_index, ProgramOptions *opts, short is_rea
     else
         fp = fopen(opts->filename, "r+");
     if(!fp){
-        fprintf(stderr, "Error while opening the file.\n");
+        fprintf(stderr, "timed_posix_read_write: error while opening the file.\n");
         exit(IO_ERROR);
     }
-    if(fseek(fp, opts->block_size * block_index, SEEK_SET) != 0){
-        fprintf(stderr, "Error while moving within the file.\n");
+    if(fseek(fp, (long)opts->block_size * block_index, SEEK_SET) != 0){
+        fprintf(stderr, "timed_posix_read_write: error while moving within the file.\n");
         fclose(fp);
         exit(IO_ERROR);
     }
     char *buffer = malloc(sizeof(char) * opts->block_size);
     if(!buffer){
-        fprintf(stderr, "Error while allocating memory.\n");
+        fprintf(stderr, "timed_posix_read_write: error while allocating memory.\n");
         fclose(fp);
         exit(MEMORY_ERROR);
     }
@@ -81,7 +82,7 @@ float timed_posix_read_write(int block_index, ProgramOptions *opts, short is_rea
         ret_val = fwrite(buffer, sizeof(char) * opts->block_size, 1, fp);
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
     if(ret_val < 1){
-        fprintf(stderr, "timed_read_write: error while writing/reading portion of the file.\n");
+        fprintf(stderr, "timed_posix_read_write: error while writing/reading portion of the file.\n");
         exit(IO_ERROR);
     }
     free(buffer);
@@ -100,15 +101,16 @@ float run_experiment(ProgramOptions *opts){
     else
         io_func = timed_posix_read_write;
     // arrays where to save timings
-    float *read_times = (float*) malloc(sizeof(float) * opts->block_count);
-    float *write_times = (float*) malloc(sizeof(float) * opts->block_count);
+    float *read_bw = (float*) malloc(sizeof(float) * opts->block_count);
+    float *write_bw = (float*) malloc(sizeof(float) * opts->block_count);
     int num_writes = 0, num_reads = 0;
-    if(!read_times || !write_times){
+    if(!read_bw || !write_bw){
         fprintf(stderr, "Error while allocating memory.\n");
         exit(MEMORY_ERROR);
     }
     int current_block;
     const int blocks_in_file = opts->file_size / opts->block_size;
+    float block_size_in_mb = opts->block_size / (1024.0f * 1024);
     // TODO: possible parallelisation here
     for(int count = 0; count < opts->block_count; count++){
         if(opts->random_access){
@@ -120,21 +122,21 @@ float run_experiment(ProgramOptions *opts){
         // TODO: set seed
         if(((float)rand())/RAND_MAX <= opts->read_prob){
             // it is a read op
-            read_times[num_reads++] = io_func(current_block, opts, READ);
+            read_bw[num_reads++] =  block_size_in_mb/io_func(current_block, opts, READ);
         }else{
             // it is a write op
-            write_times[num_writes++]= io_func(current_block, opts, WRITE);
+            write_bw[num_writes++] = block_size_in_mb/io_func(current_block, opts, WRITE);
         }
     }
 
     // stats
-    float block_size_in_mb = opts->block_size / (1024.0f * 1024);
-    float avg_read_bandwidth = block_size_in_mb / mean(read_times, num_reads);
-    float avg_write_bandwidth = block_size_in_mb / mean(write_times, num_writes);
-    printf("Average read bandwidth: %.3f MB/s (N. read ops: %.2f\%).\n", avg_read_bandwidth, (float) num_reads / (num_reads + num_writes) * 100);
-    printf("Average write bandwidth: %.3f MB/s (N. write ops: %.2f\%).\n", avg_write_bandwidth, (float) num_writes / (num_reads + num_writes) * 100);
-    free(read_times);
-    free(write_times);
+    float avg_read_bandwidth = mean(read_bw, num_reads), rd_std = stddev(read_bw, num_reads);
+    float avg_write_bandwidth = mean(write_bw, num_writes), wr_std = stddev(write_bw, num_writes);
+
+    printf("Average read bandwidth: %.3f MB/s. Standard deviation: %.3f. (N. read ops: %.2f\%).\n", avg_read_bandwidth, rd_std, (float) num_reads / (num_reads + num_writes) * 100);
+    printf("Average write bandwidth: %.3f MB/s. Standard deviation: %.3f. (N. write ops: %.2f\%).\n", avg_write_bandwidth, wr_std, (float) num_writes / (num_reads + num_writes) * 100);
+    free(read_bw);
+    free(write_bw);
 }
 
 
